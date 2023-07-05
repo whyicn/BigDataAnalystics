c7057545fff85f31818a6d3748a32756f86571e0
Hongyu <whyicn@hotmail.com>
2023-07-05 18:57:27 +0200

    RandomForestTree evaluation update

--- src/main/scala/classificationTask/RandomForestTree.scala
+++ src/main/scala/classificationTask/RandomForestTree.scala
@@ -9,6 +9,7 @@ import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 import org.apache.spark.mllib.evaluation.{BinaryClassificationMetrics, MulticlassMetrics}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, input_file_name, regexp_extract, udf}
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
 
 object RandomForestTree {
   def main(args: Array[String]): Unit = {
@@ -56,17 +57,69 @@ object RandomForestTree {
       .setEstimator(pipeline)
       .setEvaluator(evaluator)
       .setEstimatorParamMaps(paramGrid)
-      .setNumFolds(3)
+      .setNumFolds(5)
 
     val cvModel = cv.fit(personality)
 
     val bestModel = cvModel.bestModel.asInstanceOf[PipelineModel]
     val rfModel = bestModel.stages.last.asInstanceOf[RandomForestClassificationModel]
+    cvModel.transform(testData).show()
 
-    println(s"Accuracy: ${evaluator.evaluate(cvModel.transform(testData))}")
-    println(s"Number of trees: ${rfModel.getNumTrees}")
-    println(s"Max depth: ${rfModel.getMaxDepth}")
+    val predictionsAndLabels = bestModel.transform(testData).select("prediction", label)
+    predictionsAndLabels.show()
 
+    val accuracy = evaluator.evaluate(predictionsAndLabels)
+
+    val evaluator2 = new MulticlassClassificationEvaluator()
+      .setMetricName("weightedPrecision")
+      .setLabelCol(label)
+      .setPredictionCol("prediction")
+    val weightedPrecision = evaluator2.evaluate(predictionsAndLabels)
+
+    val evaluator3 = new MulticlassClassificationEvaluator()
+      .setMetricName("weightedRecall")
+      .setLabelCol(label)
+      .setPredictionCol("prediction")
+    val weightedRecall = evaluator3.evaluate(predictionsAndLabels)
+
+    val evaluator4 = new MulticlassClassificationEvaluator()
+      .setMetricName("f1")
+      .setLabelCol(label)
+      .setPredictionCol("prediction")
+    val f1 = evaluator4.evaluate(predictionsAndLabels)
+
+
+    val evaluator5 = new BinaryClassificationEvaluator()
+      .setLabelCol(label)
+      .setRawPredictionCol("prediction")
+      .setMetricName("areaUnderROC")
+    val areaUnderROC = evaluator5.evaluate(predictionsAndLabels)
+
+
+    // Extract the scores and labels from the predictions
+    val scoresAndLabels = predictionsAndLabels.select("probability", label)
+      .rdd.map(row => (row.getAs[org.apache.spark.ml.linalg.Vector](0)(1), row.getDouble(1)))
+
+    // Instantiate a BinaryClassificationMetrics object
+    val metrics = new BinaryClassificationMetrics(scoresAndLabels)
+
+    // Compute the ROC curve for each label
+    val rocCurves = metrics.roc()
+
+    // Print the ROC curve for each label
+    for (i <- 0 until 3) {
+      //todo:println(s"ROC curve for label $i: ${rocCurves(i).collect().toList}")
+    }
+
+    println(s"Accuracy: $accuracy")
+    println(s"weightedPrecision: $weightedPrecision")
+    println(s"weightedRecall: $weightedRecall")
+    println(s"f1: $f1")
+    println(s"areaUnderROC: $areaUnderROC")
+    println(s"Number of trees: $rfModel.getNumTrees")
+    println(s"Max depth: $rfModel.getMaxDepth")
+
+    //bestModel.write.overwrite().save("model")
     spark.stop()
 
   }
