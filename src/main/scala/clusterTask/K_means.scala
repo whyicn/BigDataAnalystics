dc562bdc6694c714f726d154db80240db7698462
Hongyu <whyicn@hotmail.com>
2023-07-07 15:05:03 +0200

    K-means

--- src/main/scala/clusterTask/K_means.scala
+++ src/main/scala/clusterTask/K_means.scala
@@ -2,9 +2,9 @@ package clusterTask
 
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.ml.clustering.{BisectingKMeans, GaussianMixture, KMeans}
 import org.apache.spark.ml.evaluation.ClusteringEvaluator
-object GaussianMixture {
+object K_means {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
       .builder()
@@ -17,7 +17,7 @@ object GaussianMixture {
     val personality = spark.read.format("csv")
       .option("header", "true")
       .option("inferSchema", "true")
-      .load("dataset/marketing_campaign_cleaned_cluster_balance.csv")
+      .load("dataset/marketing_campaign_cleaned_cluster_balance_1.csv")
 
     val label = "Income"
 
@@ -27,28 +27,23 @@ object GaussianMixture {
 
     val data = assembler.transform(personality).select("features")
 
+    // Trains a bisecting k-means model.
+    val bkm = new BisectingKMeans().setK(3).setSeed(123)
+    val model = bkm.fit(data)
 
+    // Make predictions
+    val predictions = model.transform(data)
 
-      val kmeans = new KMeans()
-        .setK(2)
-        .setSeed(1L)
+    // Evaluate clustering by computing Silhouette score
+    val evaluator = new ClusteringEvaluator()
 
-      val model = kmeans.fit(data)
-      val predictions = model.transform(data)
+    val silhouette = evaluator.evaluate(predictions)
+    println(s"Silhouette with squared euclidean distance = $silhouette")
 
-      val evaluator = new ClusteringEvaluator()
-        .setDistanceMeasure("squaredEuclidean")
-        .setPredictionCol("prediction")
-
-      val silhouette = evaluator.evaluate(predictions)
-
-      println(s"Silhouette with squared euclidean distance = $silhouette")
-
-      val dbIndex = evaluator.evaluate(predictions)
-      println(s"Davies-Bouldin index = $dbIndex")
-
-      val chIndex = evaluator.evaluate(predictions)
-      println(s"Calinski-Harabasz index = $chIndex")
+    // Shows the result.
+    println("Cluster Centers: ")
+    val centers = model.clusterCenters
+    centers.foreach(println)
 
 
   }
